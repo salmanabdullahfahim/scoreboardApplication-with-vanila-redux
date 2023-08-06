a8a0d73da28b3a3770548fc1acf29342d825a896
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-08-06 15:10:57 +0600

    select necessary dom element

---
+++ html/app.js
@@ -0,0 +1,6 @@
+// Selection of DOM Element 
+
+const deleteBtns = document.querySelectorAll('.lws-delete')
+const resetBtn = document.getElementById("resetBtn");
+const addAnotherMatchBtn = document.getElementById("addMatchBtn");
+const allMatchesContainer = document.getElementById('all-matches-containers')
