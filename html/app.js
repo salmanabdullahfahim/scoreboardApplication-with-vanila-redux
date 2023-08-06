6c98f0978482f02a9828bd93e282f6b97446f176
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-08-06 15:19:44 +0600

    action creator added

--- html/app.js
+++ html/app.js
@@ -3,4 +3,39 @@
 const deleteBtns = document.querySelectorAll('.lws-delete')
 const resetBtn = document.getElementById("resetBtn");
 const addAnotherMatchBtn = document.getElementById("addMatchBtn");
-const allMatchesContainer = document.getElementById('all-matches-containers')
+const allMatchesContainer = document.getElementById('all-matches-containers');
+
+
+// Action creator
+
+const increment = (payload) => {
+    return {
+        type: INCREMENT,
+        payload
+    }
+}
+
+const decrement = (payload) => {
+    return {
+        type: DECREMENT,
+        payload
+    }
+}
+
+const addMatch = () => {
+    return {
+        type: ADDAMATCH
+    }
+}
+
+const deleteMatch = () => {
+    return {
+        type: DELETEMATCH
+    }
+}
+
+const reset = () => {
+    return {
+        type: RESET
+    }
+}
